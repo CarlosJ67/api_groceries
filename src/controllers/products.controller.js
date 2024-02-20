f4e336a3e8442cd531221e4a5cbe147138ee248e
Carlos Jesus Carballo Cruz <149464565+CarlosJ67@users.noreply.github.com>
2024-02-20 15:50:55 -0600

    Update products.controller.js

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,55 +1,48 @@
 import productDao from "../dao/products.dao.js";
 
-export const getAll=(req, res)=>{
-    productDao.getAll() //Este devuelve una promesa
-    .then(products=> res.render('../src/views/index', {products}))
-    .catch(err=> res.json({
-        status: "Server unavailable"
-    }));
-}
-export const getOne=(req, res)=>{
-        productDao.getOne(req.params.barcode) //Este devuelve una promesa
-        .then(product=> {
-            !product ? res.json({
-                message: "Product not found :/"
-            }) : res.render('../src/views/edit', {product});
-        })
-        .catch(err=> res.json({
+export const getAll = (req, res) => {
+    productDao.getAll() // Este devuelve una promesa
+        .then(products => res.json({ products }))
+        .catch(err => res.json({
             status: "Server unavailable"
         }));
+}
 
-    };
-
-
-
-    export const insertOne=(req, res)=>{
-        productDao.insertOne(req.body)
-        .then(result=>res.redirect('/'))        
-        .catch(err=> res.json({status: "Server unavailable"}));
-    };
-
-
-
-
-    export const updateOne = (req, res) => {
-        productDao.updateOne(req.params.barcode, req.body)
-          .then((product) => {
-            !product ? res.console({
-                  message: "product not found",
-                })
-              : res.redirect('/');
-          })
-          .catch((err) => res.console({ status: "Server unavaliable=/"}));
-      };
-
-    
-    export const deleteOne=(req, res)=>{
-        productDao.deleteOne(req.params.barcode)
-        .then(product=>{
+export const getOne = (req, res) => {
+    productDao.getOne(req.params.barcode) // Este devuelve una promesa
+        .then(product => {
             !product ? res.json({
                 message: "Product not found :/"
-            }) : res.redirect('/')
+            }) : res.json({ product });
         })
+        .catch(err => res.json({
+            status: "Server unavailable"
+        }));
+};
 
-        .catch(err=>res.json({status: "Server unavailable"}));
-    };
+export const insertOne = (req, res) => {
+    productDao.insertOne(req.body)
+        .then(result => res.json({ message: "Insertion successful" }))
+        .catch(err => res.json({ status: "Server unavailable" }));
+};
+
+export const updateOne = (req, res) => {
+    productDao.updateOne(req.params.barcode, req.body)
+        .then((product) => {
+            !product ? res.json({
+                message: "Product not found :/"
+            }) : res.json({ message: "Update successful" });
+        })
+        .catch((err) => res.json({ status: "Server unavailable" }));
+};
+
+export const deleteOne = (req, res) => {
+    productDao.deleteOne(req.params.barcode)
+        .then(product => {
+            !product ? res.json({
+                message: "Product not found :/"
+            }) : res.json({ message: "Deletion successful" });
+        })
+        .catch(err => res.json({ status: "Server unavailable" }));
+};
+
